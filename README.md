16f730e16c020f5f512421ce05f1eece38733182
joates <github@joat.es>
2016-08-31 13:27:09 +0100

    improved code for routes (http-server module)

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-#### hyper-webgl-rs
+## hyper-webgl-rs
 
 ```terrain project ...```
+
+#### getting started
+
+__Important note:__
+The Rust language must be installed on your system (see [Installing Rust](https://doc.rust-lang.org/book/getting-started.html))
+
+* ```git clone https://github.com/joates/hyper-webgl-rs.git hyper_webgl_rs```
+* ```cd hyper_webgl_rs```
+* ```cargo run```
+
+Once the server is started, the demo application is located on your local system at [localhost:8080](http://localhost:8080/)
--- src/http_server/mod.rs
+++ src/http_server/mod.rs
@@ -16,78 +16,56 @@ pub fn start() {
     });
 }
 
+// The HTTP server handler
+fn handler(req: Request, mut res: Response) {
+    let (body, mime) = match &*req.uri.to_string() {
+
+        // permitted routes
+        "/js/three.min.js" => load_js("three.min.js"),
+        "/js/OrbitControls.js" => load_js("OrbitControls.js"),
+
+        // all other route requests return `index.html`
+        _ => load_index()
+    };
+    res.headers_mut().set(ContentType(mime));
+    let mut res = res.start().unwrap();
+    res.write_all(body.as_bytes()).unwrap();
+    res.end().unwrap();
+}
+
+fn load_index() -> (String, Mime) {
+    println!("\trequest: public/index.html");
+
+    let body = match get_file(String::from("public/index.html")) {
+        Ok(b) => b,
+        Err(e) => {
+            format!("Read file error: {}", e)
+        }
+    };
+    let mime_html = Mime(TopLevel::Text, SubLevel::Html,
+                         vec![(Attr::Charset, Value::Utf8)]
+    );
+    (body, mime_html)
+}
+
+fn load_js(file: &str) -> (String, Mime) {
+    println!("\trequest: public/js/{}", file);
+
+    let body = match get_file("public/js/".to_string() + file) {
+        Ok(b) => b,
+        Err(e) => {
+            format!("Read file error: {}", e)
+        }
+    };
+    let mime_js = Mime(TopLevel::Application, SubLevel::Javascript,
+                         vec![(Attr::Charset, Value::Utf8)]
+    );
+    (body, mime_js)
+}
+
 fn get_file(file_name: String) -> Result<String, io::Error> {
     let mut file = try!(File::open(file_name));
     let mut body = String::new();
     try!(file.read_to_string(&mut body));
-
     Ok(body)
-}
-
-// The HTTP server handler
-fn handler(req: Request, mut res: Response) {
-
-    let (body, mime) = match &*req.uri.to_string() {
-
-        // permitted routes
-        "/" | "/index.html" => {
-            println!("\trequest: public/index.html");
-
-            let _body = match get_file(String::from("public/index.html")) {
-                Ok(b) => b,
-                Err(e) => {
-                    format!("Read file error: {}", e)
-                }
-            };
-
-            let _mime = Mime(TopLevel::Text, SubLevel::Html,
-                                 vec![(Attr::Charset, Value::Utf8)]
-            );
-
-            (_body, _mime)
-        }
-
-        "/js/three.min.js" => {
-            println!("\trequest: public/js/three.min.js");
-
-            let _body = match get_file(String::from("public/js/three.min.js")) {
-                Ok(b) => b,
-                Err(e) => {
-                    format!("Read file error: {}", e)
-                }
-            };
-
-            let _mime = Mime(TopLevel::Application, SubLevel::Javascript,
-                                 vec![(Attr::Charset, Value::Utf8)]
-            );
-
-            (_body, _mime)
-        }
-
-        "/js/OrbitControls.js" => {
-            println!("\trequest: public/js/OrbitControls.js");
-
-            let _body = match get_file(String::from("public/js/OrbitControls.js")) {
-                Ok(b) => b,
-                Err(e) => {
-                    format!("Read file error: {}", e)
-                }
-            };
-
-            let _mime = Mime(TopLevel::Application, SubLevel::Javascript,
-                                 vec![(Attr::Charset, Value::Utf8)]
-            );
-
-            (_body, _mime)
-        }
-
-        _ => { (String::from("oops !"), Mime(TopLevel::Text, SubLevel::Plain,
-                                 vec![(Attr::Charset, Value::Utf8)])) }
-    };
-
-    res.headers_mut().set(ContentType(mime));
-    let mut res = res.start().unwrap();
-    res.write_all(body.as_bytes()).unwrap();
-    //println!("\t{:?}", &res.headers());
-    res.end().unwrap();
 }
